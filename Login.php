34b509b4fdb4d8efa19f337fab8e865fe62bafdd
yojia <sh93211@gmail.com>
2014-07-21 08:43:51 +0800

    Login build success

--- Login.php
+++ Login.php
@@ -4,11 +4,20 @@
 	$objDB = mysql_select_db("user_data");
 	mysql_query ( "set character set utf8" );
 
-	$user_name = $_POST["user_name"];
-	$user_password = $_POST["user_password"];
+	$user_name = $_POST["username"];
+	$user_password = $_POST["password"];
+	$device_token = $_POST["device_token"];
+	$device_os = $_POST["device_os"];
 	
-	$select_action = "SELECT * FROM user_table WHERE user_name = '$user_name'  
-		AND user_password = '$user_password'  
+	$query = sprintf("UPDATE `user_table` SET 
+					`device_token` = '$device_token',
+					`device_os` = '$device_os' 
+					WHERE `username` = '$user_name';");
+
+	$res = mysql_query($query,$objConnect);
+
+	$select_action = "SELECT * FROM user_table WHERE username = '$user_name'  
+		AND password = '$user_password'  
 		";
 
 	$objQuery = mysql_query($select_action);
@@ -20,7 +29,7 @@
 
 		$response_result = FALSE;
 		$arr["result"] = $response_result;
-		$arr["errorMessage"] = "帳號或密碼輸入錯誤";
+		$arr["Message"] = "帳號或密碼輸入錯誤";
 		
 		echo json_encode($arr);
 		exit();
@@ -29,11 +38,15 @@
 	{
 		$response_result = TRUE;
 		$arr["result"] = $response_result;
-		$arr["user_name"] = $user_name;
-		$arr["user_password"] = $user_password;
-		$arr["name"] = $objResult["name"];
-		$arr["job"] = $objResult["job"];
+
 		$arr["Message"] = "登入成功";
+
+		$arr["username"] = $objResult["username"];
+		$arr["password"] = $objResult["password"];
+		$arr["name"] = $objResult["name"];
+		$arr["user_id"] = $objResult["user_id"];
+		$arr["device_token"] = $objResult["device_token"];
+		$arr["device_os"] = $objResult["device_os"];
 
 		echo json_encode($arr);
 		exit();
