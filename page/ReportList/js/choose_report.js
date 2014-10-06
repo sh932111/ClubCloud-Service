4893c6e88aaca20fb57311b7d395cb3dea618b50
yojia <sh93211@gmail.com>
2014-10-06 14:09:11 +0800

    update

--- page/ReportList/js/choose_report.js
+++ page/ReportList/js/choose_report.js
@@ -4,5 +4,5 @@ function init()
 }
 function emList()
 {
-	
+	document.getElementById('iframe').setAttribute('src','page/emergency_list.php?username='+userName);
 }
---
+++ page/ReportList/page/emergency_list.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html >
+
+<?php
+
+$username = $_GET['username'];
+
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript">
+
+var userName = '<?php echo $username; ?>';
+//console.log(userName);
+</script>
+
+<title>緊急事件清單</title>
+<head>
+	<link rel="stylesheet" href="css/emergency_style.css">
+</head>
+<body onload="init()">
+	
+</body>
+
+<script src="js/emergency.js"></script>
+</html>
