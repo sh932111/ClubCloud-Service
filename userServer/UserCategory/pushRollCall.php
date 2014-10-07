9eb84bad29e19b91ee49c02f6b18f217d8bfc386
yojia <sh93211@gmail.com>
2014-10-07 21:08:27 +0800

    update

---
+++ userServer/UserCategory/pushRollCall.php
@@ -0,0 +1,101 @@
+<?php
+header('Content-Type: text/html; charset=utf8');
+
+mysql_query("SET NAMES 'utf8'");
+mysql_query("SET CHARACTER_SET_CLIENT='utf8'");
+mysql_query("SET CHARACTER_SET_RESULTS='utf8'");
+
+$link = mysql_connect('localhost','root','sh3599033');
+mysql_query("SET NAMES 'utf8'",$link);
+
+$username = $_POST["username"];
+$id = $_POST["id"];
+
+if (!$link) 
+{
+	$arr["result"] = FALSE;
+	$arr["Message"] = "初始化失敗";
+	echo json_encode($arr);
+	exit();
+}
+
+$db_selected = mysql_select_db('UserCategory');
+
+if (!$db_selected)
+{
+	$arr["result"] = FALSE;
+	$arr["Message"] = "error select db";
+	echo json_encode($arr);
+	exit();
+}
+else
+{
+	$creat_query  ="CREATE TABLE `$username`(
+		`id` VARCHAR(200) NOT NULL PRIMARY KEY
+		);";
+
+	$table_selected = mysql_query($creat_query, $link);
+
+	$user_query = sprintf("INSERT INTO `$username`(`id`) 
+                VALUES ('%s')",
+                $id);
+
+    $user_query_res = mysql_query($user_query,$link);
+
+    if ($user_query_res) 
+    {
+    	//去user_data 取資料
+
+    	$db_selected2 = mysql_select_db('user_data');
+
+    	$select_action2 = "SELECT * FROM calendar_table WHERE data_id = '$id'";
+
+		$objQuery = mysql_query($select_action2);
+		$objResult = mysql_fetch_array($objQuery);
+		$intNumRows = mysql_num_rows($objQuery);
+	    
+		if ($intNumRows == 0) 
+		{
+			$response_result = FALSE;
+			$arr["result"] = $response_result;
+			$arr["Message"] = "無此活動資料！";
+			
+			echo json_encode($arr);
+			exit();
+		}
+		else
+		{
+			$data["name"] = $objResult["name"];
+			$data["username"] = $objResult["username"];
+			$data["data_id"] = $objResult["data_id"];
+			$data["title"] = $objResult["title"];
+			$data["detail"] = $objResult["detail"];
+			$data["date"] = $objResult["date"];
+			
+			$data["time"] = $objResult["time"];//territory_name
+			$data["city"] = $objResult["city"];//district_name
+			$data["area"] = $objResult["area"];//city_id
+			$data["liner"] = $objResult["liner"];//district_id
+			
+			$data["address"] = $objResult["address"];//district_id
+			$data["image"] = $objResult["image"];//district_id
+		
+			$arr["data"] = $data;
+
+		    $arr["result"] = TRUE;
+			$arr["Message"] = "點名成功!";
+			echo json_encode($arr);
+			exit();
+		}
+    }
+    else
+    {
+    	$arr["result"] = FALSE;
+		$arr["Message"] = "點名失敗，可能已經點過名";
+		echo json_encode($arr);
+		exit();
+    }
+}
+mysql_close($link);
+
+?>
