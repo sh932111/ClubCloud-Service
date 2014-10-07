97814d2252c339151e218105774f9a2a4dba0c7b
yojia <sh93211@gmail.com>
2014-10-07 11:25:59 +0800

    update

--- page/ReportList/page/js/emergency_detail.js
+++ page/ReportList/page/js/emergency_detail.js
@@ -1,4 +1,5 @@
 var userData;
+var responseData;
 
 function init()
 {
@@ -39,14 +40,71 @@ function getDetail()
         {
             var return_data = xmlhttp.responseText;
 
-            var get_json = JSON.parse(return_data);
+            responseData = JSON.parse(return_data);
 
-            console.log(get_json);
+            if (responseData.result)
+            {
+                if (responseData.data.num != 0)
+                {
+                    for (var i = 0; i < responseData.data.num; i++) 
+                    {
+                        var obj = responseData.data[i];
+
+                        var listView = document.getElementById('listView');
+
+                        var nameDiv = document.createElement("div");
+
+                        nameDiv.className = '.user';
+                        
+                        nameDiv.innerHTML = obj.name;
+
+                        var idDiv = document.createElement("div");
+
+                        idDiv.className = '.user_id';
+                        
+                        idDiv.innerHTML = obj.user_id;
+
+                        var latDiv = document.createElement("div");
+
+                        latDiv.className = '.latitude';
+                        
+                        latDiv.innerHTML = obj.latitude;
+
+                        var longDiv = document.createElement("div");
+
+                        longDiv.className = '.longitude';
+                        
+                        longDiv.innerHTML = obj.longitude;
+
+                        var checkDiv = document.createElement("div");
+
+                        checkDiv.className = '.check';
+                        
+                        if (obj.t_check == 0)
+                        {
+                            checkDiv.innerHTML = "未回報";
+                        }
+                        else if (obj.t_check == 1)
+                        {
+                            checkDiv.innerHTML = "手機自行回報";
+                        }
+                        else if (obj.t_check == 2)
+                        {
+                            checkDiv.innerHTML = "使用者已回報";
+                        }
+
+                        listView.appendChild(nameDiv);
+                        listView.appendChild(idDiv);
+                        listView.appendChild(latDiv);
+                        listView.appendChild(longDiv);
+                        listView.appendChild(checkDiv);
+                    }
+                }
+            }
+
         }
     }
     // Send the data to PHP now... and wait for response to update the status div
     var post = "postId="+eventID;
-
-    console.log(post);
     xmlhttp.send(post); 
 }
