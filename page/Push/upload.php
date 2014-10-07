3519f1a991f7250b07b2249aed6972bf55336889
yojia <sh93211@gmail.com>
2014-10-07 20:19:56 +0800

    上傳圖片測試

--- page/Push/upload.php
+++ page/Push/upload.php
@@ -1,5 +1,7 @@
 <?php 
 
+include('../lib/class.upload.php');
+
 $cover = $_FILES["pic"];
 
 $fileid= $_POST["item"];
@@ -38,4 +40,34 @@ else
             // err file type
     }
 }
+$old_file=_UPLOAD_DIR."/{$fileid}.png";
+
+if(file_exists($old_file))
+{
+    unlink($old_file);
+}
+
+$img_handle = new upload($_FILES['pic'],"zh_TW");
+
+if ($img_handle->uploaded) 
+{
+    $img_handle->file_safe_name = false;
+    $img_handle->file_overwrite  = true;
+    $img_handle->file_new_name_body   = $sn;
+    $img_handle->image_convert = 'png';
+    $img_handle->image_resize  = true;
+    $img_handle->image_ratio_y  = true;
+    $img_handle->image_ratio_x  = true;
+    $img_handle->process($target_path);
+
+    if ($img_handle->processed) 
+    {
+        $img_handle->clean();
+    } 
+    else 
+    {
+        die($img_handle->error);
+    }
+}
+
 ?>
