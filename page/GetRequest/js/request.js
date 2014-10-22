70cf7c6b693e5703602df8c08eb568d0cd2309b4
yojia <sh93211@gmail.com>
2014-10-22 14:02:29 +0800

    update

--- page/GetRequest/js/request.js
+++ page/GetRequest/js/request.js
@@ -57,7 +57,7 @@ function getRequestData()
 
         var list = document.getElementById("msg3");
 
-        var result = "<table border='1'>";
+        var result = "<table border='0'>";
 
         result += "<tr><td>發送人</td><td>帳號</td><td>標題</td><td>日期</td><td>時間</td><td>前往</td></tr>";
 
@@ -69,14 +69,13 @@ function getRequestData()
             if (city_id == cityId && area_id == areaId)
             {
                 
-
-            var name  = get_json[i]["name"];            
-            var username  = get_json[i]["username"];            
-            var title  = get_json[i]["title"];            
-            var date  = get_json[i]["date"];            
-            var time  = get_json[i]["time"];            
-                
-            result += "<tr><td>" + name + "</td><td>" + username + "</td><td>" + title + "</td><td>" + date + "</td><td>"+ time  + "</td><td><button onclick='goPage(this)' id="+i+">go</button></td></tr>";
+                var name  = get_json[i]["name"];            
+                var username  = get_json[i]["username"];            
+                var title  = get_json[i]["title"];            
+                var date  = get_json[i]["date"];            
+                var time  = get_json[i]["time"];            
+                    
+                result += "<tr><td>" + name + "</td><td>" + username + "</td><td>" + title + "</td><td>" + date + "</td><td>"+ time  + "</td><td><button onclick='goPage(this)' id="+i+">go</button></td></tr>";
             }
         }
         result += "</table>";
