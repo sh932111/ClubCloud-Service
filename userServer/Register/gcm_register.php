34bf2b6268701ee345789931b86f6ae7cb8e36a8
yojia <sh93211@gmail.com>
2014-10-25 07:34:31 +0800

    update

--- userServer/Register/gcm_register.php
+++ userServer/Register/gcm_register.php
@@ -11,34 +11,37 @@ mysql_query("SET NAMES 'utf8'",$link);
 $userName = $_POST["username"];
 $regId = $_POST['regId'];
 
-if (!$link) 
+if (strlen( $userName) > 0 ) 
 {
-	$arr["result"] = FALSE;
-	$arr["Message"] = "初始化失敗";
-	echo json_encode($arr);
-	exit();
+
+	if (!$link) 
+	{
+		$arr["result"] = FALSE;
+		$arr["Message"] = "初始化失敗";
+		echo json_encode($arr);
+		exit();
+	}
+	$db_selected = mysql_select_db('user_data');
+
+	if (!$db_selected)
+	{
+		$arr["result"] = FALSE;		
+
+		$arr["Message"] = "抓取資料失敗";
+
+		echo json_encode($arr);
+		exit();
+	}
+	else
+	{
+		$query = sprintf("UPDATE `user_table` SET 
+			`device_token` = '$regId'
+			WHERE `username` = '$userName';");
+		$res = mysql_query($query,$objConnect);
+
+		mysql_close($objConnect);
+
+	}
 }
-$db_selected = mysql_select_db('user_data');
-
-if (!$db_selected)
-{
-	$arr["result"] = FALSE;		
-
-	$arr["Message"] = "抓取資料失敗";
-
-	echo json_encode($arr);
-	exit();
-}
-else
-{
-	$query = sprintf("UPDATE `user_table` SET 
-					`device_token` = '$regId'
-					WHERE `username` = '$userName';");
-	$res = mysql_query($query,$objConnect);
-
-	mysql_close($objConnect);
-
-}
-
 
 ?>
