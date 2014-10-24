7403ef12017b2fb8dcde2577d2ae5c6a06d0b4d1
yojia <sh93211@gmail.com>
2014-10-25 07:28:28 +0800

    update

--- userServer/Register/gcm_register.php
+++ userServer/Register/gcm_register.php
@@ -1,10 +1,44 @@
 <?php
+header('Content-Type: text/html; charset=utf8');
 
-if(isset($_POST['regId']))
+mysql_query("SET NAMES 'utf8'");
+mysql_query("SET CHARACTER_SET_CLIENT='utf8'");
+mysql_query("SET CHARACTER_SET_RESULTS='utf8'");
+
+$link = mysql_connect('localhost','root','sh3599033');
+mysql_query("SET NAMES 'utf8'",$link);
+
+$userName = $_POST["username"];
+$regId = $_POST['regId'];
+
+if (!$link) 
 {
-    $regId = $_POST['regId'];
-    // $sql = "INSERT INTO 資料表 (gcm_id,...) VALUES ('$regId',...)";
-    // $pdo->exec($sql);
+	$arr["result"] = FALSE;
+	$arr["Message"] = "初始化失敗";
+	echo json_encode($arr);
+	exit();
 }
+$db_selected = mysql_select_db('user_data');
+
+if (!$db_selected)
+{
+	$arr["result"] = FALSE;		
+
+	$arr["Message"] = "抓取資料失敗";
+
+	echo json_encode($arr);
+	exit();
+}
+else
+{
+	$query = sprintf("UPDATE `user_table` SET 
+					`device_token` = '$regId'
+					WHERE `username` = '$userName';");
+	$res = mysql_query($query,$objConnect);
+
+	mysql_close($objConnect);
+
+}
+
 
 ?>
