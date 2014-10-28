03ea5a442dbba18ee8b34c2bdc196c7ec4839395
yojia <sh93211@gmail.com>
2014-10-28 20:10:20 +0800

    update

--- page/Calender/php/push_calendar.php
+++ page/Calender/php/push_calendar.php
@@ -29,6 +29,10 @@ $address_city = $_POST["address_city"];
 $address_area = $_POST["address_area"];
 $send_time = $_POST["send_time"];
 
+$sql = 'CREATE DATABASE calendar_data';
+
+mysql_query($sql, $link);
+
 if (!$objConnect) 
 {
 	$arr["result"] = FALSE;
@@ -69,28 +73,67 @@ if ($db_selected)
 		`send_time` VARCHAR(100) NOT NULL
 		);";
 
-$table_selected = mysql_query($creat_query, $objConnect);
+	$table_selected = mysql_query($creat_query, $objConnect);
 
-$query = sprintf("INSERT INTO `calendar_table`(`data_id`,`name`,`username`,`city_id`,`area_id`,`title`,`detail`,`date`,`time`,`city`,`area`,`liner`,`address`,`image`,`address_city`,`address_area`,`send_time`) 
-	VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')",
-	$id,$name,$username,$city_id,$area_id,$title,$detail,$date,$time,$city,$area,$liner,$address,$image,$address_city,$address_area,$send_time);
+	$query = sprintf("INSERT INTO `calendar_table`(`data_id`,`name`,`username`,`city_id`,`area_id`,`title`,`detail`,`date`,`time`,`city`,`area`,`liner`,`address`,`image`,`address_city`,`address_area`,`send_time`) 
+		VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')",
+		$id,$name,$username,$city_id,$area_id,$title,$detail,$date,$time,$city,$area,$liner,$address,$image,$address_city,$address_area,$send_time);
 
-$res = mysql_query($query,$objConnect);
+	$res = mysql_query($query,$objConnect);
 
-if (!$res) 
-{
-	$arr["Message"] = '新增訊息失敗';
-	$arr["result"] = FALSE;
-	echo json_encode($arr);
-	exit();
-}
-else
-{
-	$arr["Message"] = '新增訊息成功！';
-	$arr["result"] = true;
-	echo json_encode($arr);
-	exit();
-}
+	if (!$res) 
+	{
+		$arr["Message"] = '新增訊息失敗';
+		$arr["result"] = FALSE;
+		echo json_encode($arr);
+		exit();
+	}
+	else
+	{
+
+		$db_selected_2 = mysql_select_db('calendar_data');
+
+		$sq_creat_query  ="CREATE TABLE `$id`(
+            `username` VARCHAR(200) NOT NULL PRIMARY KEY,
+            `name` VARCHAR(200)CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `user_id` VARCHAR(200) NOT NULL,
+            `t_check` VARCHAR(20) NOT NULL,
+            `cellphone` VARCHAR(20) NOT NULL
+            );";
+
+	    $db_table_selected = mysql_query($sq_creat_query, $link);
+
+	    $db_selected_3 = mysql_select_db('user_data');
+
+        // $us_select_action = "SELECT * FROM user_table";
+        $us_select_action = "SELECT * FROM user_table WHERE city_id = '$city_id'  
+        AND city_detail_id = '$area_id'  
+        ";
+
+        $obj_Query = mysql_query($us_select_action);
+    
+        $intNumRows = mysql_num_rows($obj_Query);
+        
+        $arr = array();
+
+        if($intNumRows != 0)
+        {
+            while ($record = mysql_fetch_array($obj_Query)) 
+            {
+                $db_selected_4 = mysql_select_db('calendar_data');
+
+                $user_query = sprintf("INSERT INTO `$id`(`username`,`name`,`user_id`,`t_check`,`cellphone`) 
+                VALUES ('%s','%s','%s','%s','%s')",
+                $record["username"],$record["name"],$record["user_id"],"0",$record["cellphone"]);
+                $user_query_res = mysql_query($user_query,$link);
+            }
+        } 
+
+		$arr["Message"] = '新增訊息成功！';
+		$arr["result"] = true;
+		echo json_encode($arr);
+		exit();
+	}
 
 
 }
